0a7669fa04ee5b9d7644e7afb2da64a60b3966ef
“NiBienvenu” <nicibienvenu@gmail.com>
2024-07-08 00:55:32 +0200

    inner join

--- src/main/kotlin/com/bienvenu/mysql/entity/EntityPost.kt
+++ src/main/kotlin/com/bienvenu/mysql/entity/EntityPost.kt
@@ -4,9 +4,9 @@ import org.ktorm.schema.*
 
 object EntityPost: Table<Nothing>(tableName = "post") {
     val id = int(name = "id").primaryKey()
-    val userId = int(name = "user_id").referenceTable
+    val user_id = int(name = "user_id")
     val title = varchar(name = "title")
     val content = text(name = "description")
-    val createdAt = timestamp(name = "created_at")
-    val updatedAt = timestamp(name = "updated_at")
+    val created_at = timestamp(name = "created_at")
+    val updated_at = timestamp(name = "updated_at")
 }
--- src/main/kotlin/com/bienvenu/route/RoutePost.kt
+++ src/main/kotlin/com/bienvenu/route/RoutePost.kt
@@ -1,12 +1,16 @@
 package com.bienvenu.route
 
+import com.bienvenu.model.Post
 import com.bienvenu.mysql.DbConnection
+import com.bienvenu.mysql.entity.EntityPost
+import com.bienvenu.mysql.entity.EntityUser
 import com.bienvenu.util.GenericResponse
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.ktorm.database.Database
+import org.ktorm.dsl.*
 
 fun Application.routePost(){
     val db :Database = DbConnection.getDatabaseInstance()
@@ -14,9 +18,18 @@ fun Application.routePost(){
     routing {
         get("/getpost") {
 
+            val post = db
+                .from(EntityPost)
+                .innerJoin(EntityUser, on = EntityPost.user_id eq EntityUser.id)
+                .select()
+                post.map {
+                    println("post ........")
+                    val row = it[EntityPost.user_id]
+                  println("row -=> $row")
+                }
             call.respond(
                 HttpStatusCode.OK,
-                GenericResponse(isSuccess = true, data = "OK")
+                GenericResponse(isSuccess = true, data ="ok")
             )
         }
 
