59750aee5fc2373ede0541476d0a9345b374e966
“NiBienvenu” <nicibienvenu@gmail.com>
2024-07-10 23:58:04 +0200

    methode show and create

--- src/main/kotlin/com/bienvenu/route/RoutePost.kt
+++ src/main/kotlin/com/bienvenu/route/RoutePost.kt
@@ -7,6 +7,7 @@ import com.bienvenu.mysql.entity.EntityUser
 import com.bienvenu.util.GenericResponse
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.ktorm.database.Database
@@ -30,6 +31,45 @@ fun Application.routePost(){
             call.respond(
                 HttpStatusCode.OK,
                 GenericResponse(isSuccess = true, data ="ok")
+            )
+        }
+        get("/getpost/{id}") {
+            val pararmId = call.parameters["id"]
+            val id = pararmId?.toInt()?: -1
+
+            val post = db
+               .from(EntityPost)
+               .innerJoin(EntityUser, on = EntityPost.user_id eq EntityUser.id)
+               .select()
+               .where { EntityPost.id eq id }
+               .limit(1)
+            call.respond(
+                HttpStatusCode.OK,
+                GenericResponse(isSuccess = true, data = Post(
+//                    id = post[EntityPost.id],
+//                    userId = post[EntityUser.id],
+//                    title = post[EntityPost.title],
+//                    content = post[EntityPost.content],
+//                    createdAt = post[EntityPost.created_at],
+//                    updatedAt = post[EntityPost.updated_at]
+                ))
+            )
+        }
+        post("/createpost") {
+            val post = call.receive<Post>()
+
+            val inserted = db
+               .insert(EntityPost) {
+//                    it[user_id] = post.userId
+//                    it[title] = post.title
+//                    it[content] = post.description
+//                    it[created_at] = post.createdAt
+//                    it[updated_at] = post.updatedAt
+                }
+
+            call.respond(
+                HttpStatusCode.Created,
+                GenericResponse(isSuccess = inserted > 0, data = inserted)
             )
         }
 
